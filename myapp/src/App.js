fc08945a9ebcb0c4910c9fb4bce64c01afe2bbd6
tanvirtbf <tanver018765@gmail.com>
2023-05-22 13:38:37 +0600

    App_compo_change

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,22 +5,27 @@ import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 import AddIcon from "@mui/icons-material/Add";
 import { useState } from "react";
-import Fields from "./components/Fields";
+//import Fields from "./components/Fields";
 
 function App() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [data,setData] = useState([]);
-  const addData = ()=>{
-    setData([...data,{name,email}]);
+  const [data, setData] = useState([]);
+  const addData = () => {
+    setData([...data, { name, email }]);
     setName("");
     setEmail("");
+  };
+  const removeItem = (index)=>{
+    let arr = data;
+    arr.splice(index,1)
+    setData([...arr]);
   }
   return (
     <div className="App">
       <Header></Header>
       <div className="form">
-        <Stack direction="row" spacing={2}> 
+        <Stack direction="row" spacing={2}>
           <TextField
             value={name}
             onChange={(event) => setName(event.target.value)}
@@ -38,7 +43,7 @@ function App() {
           <Button onClick={addData} color="error" variant="contained">
             <AddIcon />
           </Button>
-        </Stack> 
+        </Stack>
       </div>
       <div className="data">
         <div className="data_val">
@@ -46,13 +51,19 @@ function App() {
           <h4>email</h4>
           <h4>remove</h4>
         </div>
-        {
-          data.map((element,index)=>{
-            return(
-              <Fields key={index} name={element.name} email={element.email} index={index}></Fields>
-            )
-          })
-        }
+        {data.map((element, index) => {
+          return (
+            <div key={index} className="data_val">
+              <h4>{element.name}</h4>
+              <h4>{element.email}</h4>
+              <Stack>
+                <Button onClick={()=>removeItem(index)} variant="contained" color="error">
+                  Delete
+                </Button>
+              </Stack>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
